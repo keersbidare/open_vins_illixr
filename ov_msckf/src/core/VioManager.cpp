b7450c45db4108505e3ff999ecd329339e62433b
keersbidare <kbida003@ucr.edu>
2024-07-03 16:18:08 -0700

    commit3

--- ov_msckf/src/core/VioManager.cpp
+++ ov_msckf/src/core/VioManager.cpp
@@ -180,9 +180,11 @@ void VioManager::feed_measurement_stereo(double timestamp, cv::Mat& img0, cv::Ma
 
     // Feed our stereo trackers, if we are not doing binocular
     if(params.use_stereo) {
-        printf(RED "Measurement_stereo_doing only stereo");
+        printf(RED "Measurement_stereo_doing in stereo");
         trackFEATS->feed_stereo(timestamp, img0, img1, cam_id0, cam_id1);
     } else {
+            printf(RED "Measurement_stereo_doing in monocular");
+
 #ifdef ILLIXR_INTEGRATION
         std::thread t_l = std::thread(&TrackBase::feed_monocular, trackFEATS, boost::ref(timestamp), boost::ref(img0), boost::ref(cam_id0));
         std::thread t_r = std::thread(&TrackBase::feed_monocular, trackFEATS, boost::ref(timestamp), boost::ref(img1), boost::ref(cam_id1));
