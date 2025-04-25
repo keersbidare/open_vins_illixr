25d5f8fc6e636c548bcb9de32a5d5ec049cea6e9
keersbidare <kbida003@ucr.edu>
2025-04-25 16:10:43 -0700

    just printing the number of points-1

--- ov_core/src/track/TrackKLT.cpp
+++ ov_core/src/track/TrackKLT.cpp
@@ -691,7 +691,7 @@ void TrackKLT::perform_matching(const std::vector<cv::Mat>& img0pyr, const std::
         pts0.push_back(kpts0.at(i).pt);
         pts1.push_back(kpts1.at(i).pt);
     }
-    printf(WHITE "[SIZE OF THE FEATURES]: %.4f:- number of features\n" RESET, pts0.size()); 
+    printf(WHITE "[SIZE OF THE FEATURES]: %d :- number of features\n" , pts0.size()); 
     // If we don't have enough points for ransac just return empty
     // We set the mask to be all zeros since all points failed RANSAC
     if(pts0.size() < 10) {
