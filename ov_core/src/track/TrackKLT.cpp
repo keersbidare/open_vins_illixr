e476b1dc917e5fe3e3bd7cc305b9483f1552489e
keersbidare <kbida003@ucr.edu>
2025-04-25 14:15:17 -0700

    profiling pyramid original code-1

--- ov_core/src/track/TrackKLT.cpp
+++ ov_core/src/track/TrackKLT.cpp
@@ -466,8 +466,8 @@ void TrackKLT::feed_stereo(double timestamp, cv::Mat &img_leftin, cv::Mat &img_r
     total_db_time += db_time;
     total_time += total;
 
-    printf(CYAN "[TIME-KLT]: %.4f ms for original optical pyramid\n" RESET, histogram);
-    printf(CYAN "[TIME-KLT]: %.4f ms for edited optical pyramid\n" RESET, optical_flow);
+    printf(CYAN "[TIME-KLT]: %.4f ms for histogram\n" RESET, histogram);
+    printf(CYAN "[TIME-KLT]: %.4f ms for optical flow\n" RESET, optical_flow);
     printf(CYAN "[TIME-KLT]: %.4f ms for pyramid\n" RESET, pyramid_time);
     printf(CYAN "[TIME-KLT]: %.4f ms for detection\n" RESET, detection_time);
     printf(CYAN "[TIME-KLT]: %.4f ms for temporal klt\n" RESET, temporal_klt_time);
