ec51911dd00c3f9c101a86b64e7fa7e1fe98f59c
keersbidare <kbida003@ucr.edu>
2025-05-01 13:47:09 -0700

    trying to fix the distortion in the final output-3

--- ov_core/src/track/TrackKLT.cpp
+++ ov_core/src/track/TrackKLT.cpp
@@ -392,18 +392,18 @@ void TrackKLT::feed_stereo(double timestamp, cv::Mat &img_leftin, cv::Mat &img_r
     // Update our feature database, with theses new observations
     
     St3 = boost::posix_time::microsec_clock::local_time();
-    std::vector<FeatureUpdate> updates;
+    //std::vector<FeatureUpdate> updates;
 
     double max_undistort_left = 0, max_undistort_right = 0;
     double max_vector_left = 0, max_vector_right = 0;
-    std::vector<FeatureUpdate> thread_local_updates_left;
-    std::vector<FeatureUpdate> thread_local_updates_right;
+    
     #pragma omp parallel
    {
     
     double local_undistort_left = 0, local_undistort_right = 0;
     double local_vector_left = 0, local_vector_right = 0;
-
+    std::vector<FeatureUpdate> thread_local_updates_left;
+    std::vector<FeatureUpdate> thread_local_updates_right;
     #pragma omp for nowait
     for (size_t i = 0; i < good_left.size(); ++i) {
         auto t1 = boost::posix_time::microsec_clock::local_time();
@@ -420,10 +420,10 @@ void TrackKLT::feed_stereo(double timestamp, cv::Mat &img_leftin, cv::Mat &img_r
         local_vector_left = std::max(local_vector_left, (t3 - t2).total_microseconds() * 1e-3);
     }
 
-    // #pragma omp critical
-    // {
-    //     database->update_features_bulk(thread_local_updates_left);
-    // }
+    #pragma omp critical
+    {
+        database->update_features_bulk(thread_local_updates_left);
+    }
 
     #pragma omp for nowait
     for (size_t i = 0; i < good_right.size(); ++i) {
@@ -441,10 +441,10 @@ void TrackKLT::feed_stereo(double timestamp, cv::Mat &img_leftin, cv::Mat &img_r
         local_vector_right = std::max(local_vector_right, (t3 - t2).total_microseconds() * 1e-3);
     }
 
-    // #pragma omp critical
-    // {
-    //     database->update_features_bulk(thread_local_updates_right);
-    // }
+    #pragma omp critical
+    {
+        database->update_features_bulk(thread_local_updates_right);
+    }
 
 
         // auto t_merge_start = boost::posix_time::microsec_clock::local_time();
@@ -466,10 +466,10 @@ void TrackKLT::feed_stereo(double timestamp, cv::Mat &img_leftin, cv::Mat &img_r
         }
    }
 
-        auto St6 = boost::posix_time::microsec_clock::local_time();
-        database->update_features_bulk(thread_local_updates_left);
-        database->update_features_bulk(thread_local_updates_right);
-        auto En3 = boost::posix_time::microsec_clock::local_time();
+        // auto St6 = boost::posix_time::microsec_clock::local_time();
+        // database->update_features_bulk(thread_local_updates_left);
+        // database->update_features_bulk(thread_local_updates_right);
+        // auto En3 = boost::posix_time::microsec_clock::local_time();
 
         // Print results
         printf(WHITE "[TIME-KLT]: %.4f ms for undistort_point_left (max per thread)\n" RESET, max_undistort_left);
